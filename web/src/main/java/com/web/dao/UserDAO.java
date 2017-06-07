a363ac80286133c48e6d611df6b2d129e3e8e2bb
TongYiming <1290863722@qq.com>
2017-06-07 11:55:32 +0800

    ac_num

--- web/src/main/java/com/web/dao/UserDAO.java
+++ web/src/main/java/com/web/dao/UserDAO.java
@@ -2,9 +2,12 @@ package com.web.dao;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by TongYiMing on 2017/6/2.
  */
 @Repository("UserDAO")
 public interface UserDAO extends BasicDao {
+    public List<Integer> getIdByAc_num();
 }
